4897190c6e267b0389f909a0f7bf1ae4ec99a3a1
Austin <Austin-from-TX>
2021-01-24 04:14:54 +0000

    user logout working

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -30,4 +30,16 @@ router.post(
   }),
 );
 
+
+// Log out
+router.delete(
+    '/',
+    (_req, res) => {
+      res.clearCookie('token');
+      return res.json({ message: 'success' });
+    }
+  );
+
+  
+
 module.exports = router;
