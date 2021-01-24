f1b77512195c7f481b15a2c57d306083baf73827
Austin <austin.smithfield@gmail.com>
2021-01-24 05:09:56 +0000

    add validation

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -56,5 +56,15 @@ router.get(
     }
   );
   
+  const validateLogin = [
+    check('credential')
+      .exists({ checkFalsy: true })
+      .notEmpty()
+      .withMessage('Please provide a valid email or username.'),
+    check('password')
+      .exists({ checkFalsy: true })
+      .withMessage('Please provide a password.'),
+    handleValidationErrors,
+  ];
 
 module.exports = router;
