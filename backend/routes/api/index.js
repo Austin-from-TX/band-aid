170f133ee515f8814040577bd0141aae81be4766
Austin <Austin-from-TX>
2021-01-24 04:08:23 +0000

    user validation working

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 
+router.use('/session', sessionRouter);
 
-router.post('/test', function(req, res) {
-    res.json({ requestBody: req.body });
-  });
+router.use('/users', usersRouter);
 
 module.exports = router;
