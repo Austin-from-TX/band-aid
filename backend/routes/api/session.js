caad6584f964f4e35bc33219f5662a676fbfd539
Austin <austin.smithfield@gmail.com>
2021-01-24 05:07:59 +0000

    test

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const asyncHandler = require('express-async-handler');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
